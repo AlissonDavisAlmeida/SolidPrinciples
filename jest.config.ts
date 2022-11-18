50787e3485c1fc429069c0280d5c19a5778847e7
alisson <alissoneatena1@gmail.com>
2022-11-18 12:00:33 -0300

    chore: add configs to jest

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+
+  roots: [
+    '<rootDir>/src'
+  ],
+
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+
+  collectCoverage: true,
+
+  coverageDirectory: 'coverage',
+
+  coverageProvider: 'v8',
+
+  testEnvironment: 'node'
+
+};
